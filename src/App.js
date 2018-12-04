e02cd3ad321d8ef4b64a62dec1ff3420f573ac09
daniel vicario <danigv.email@gmail.com>
2018-12-04 15:53:31 +0100

    updated master

--- src/App.js
+++ src/App.js
@@ -4,43 +4,7 @@ import './App.css';
 import EmployeeProfile from './EmployeeProfile'
 
 class App extends React.Component {
-  constructor() {
-    super()
-
-    this.myNiceColor = "red"
-
-    //component's state init
-    this.state = {
-      name: 'albert',
-      age: 28,
-      salary: 10000,
-      img: 'https://res.cloudinary.com/prestige-gifting/image/fetch/fl_progressive,q_95,e_sharpen:50,w_480/e_saturation:05/https://www.prestigeflowers.co.uk/images/NF1018.jpg'
-    }
-  }
-
-  //realistic reducer
-  upgradeSalary = (state) => {
-    return { ...state, salary: state.salary + 40000 }
-  }
-
-  changeButtonHandler = () => {
-    this.setState(this.upgradeSalary(this.state))
-  }
-
-  changeImageButtonHandler = () => {
-    this.setState({ ...this.state, img: 'https://m-i9.fnp.com/images/pr/uae/l/i-love-you-flower-arrangement_1.jpg' })
-  }
-
   render() {
-    const coolSnippet = (
-      <div>
-        <h1>Hello Ironhacker! {1 + 1000}</h1>
-        <h2>Good to see you here. My nice color is {this.myNiceColor}</h2>
-      </div>
-    );
-
-    
-
     const ingredients = [
       {
         name: "tomato",
@@ -60,7 +24,7 @@ class App extends React.Component {
       }
     ]
 
-    const ingredientsList = ingredients.map((ingredient, index) => <option key={ingredient.id}>{ingredient.name}</option> );
+    const ingredientsList = ingredients.map((ingredient, index) => <option key={ingredient.id}>{ingredient.name}</option>);
 
     return (
       <div className="App">
